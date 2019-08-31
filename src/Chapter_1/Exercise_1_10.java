8fe33af4c2a21c9dea9b78b0b0a94adb24085883
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-08-31 18:22:34 +0100

    Five more exercises solved from the book. @LucasVigarinho

---
+++ src/Chapter_1/Exercise_1_10.java
@@ -0,0 +1,40 @@
+package Chapter_1;
+
+
+/** 
+ * 
+ *  (Average speed in miles) Assume a runner runs 14 kilometers in 45 minutes and 30
+ *  seconds. Write a program that displays the average speed in miles per hour. (Note
+ *  that 1 mile is 1.6 kilometers.)
+ * 							
+ * */
+public class Exercise_1_10{
+
+	/** Constructor not utilized */
+	public Exercise_1_10() {
+		// TODO Auto-generated constructor stub
+	}//close constructor
+
+	/** Automatic generated main method */
+	public static void main(String[] args) {
+
+		//Creating the necessary variables 
+		double kilometer = 14;
+		double miles = kilometer / 1.6;
+		double second = 1;
+		double minute = 60 * second;
+		double hour = 60 * minute;
+
+		
+        double rate = (45.5 * minute + 30.0) / (hour);
+        double speedMiles = miles / rate;
+		
+		//printing the results out
+		System.out.println("\n\n\tThe runner was running in " + speedMiles + " miles per hour \t= " );
+
+
+		
+
+	}//close main method
+
+}//close class Exercise_1_10
