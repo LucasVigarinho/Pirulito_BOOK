4cddb1cdcaf0c656bd5f7fd7016b7717628d042b
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-09-05 13:49:29 +0100

    Chapter three - exercises 10 to 19

--- src/Chapter_3/Exercise_3_10.java
+++ src/Chapter_3/Exercise_3_10.java
@@ -1,35 +1,33 @@
 package Chapter_3;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
-//import java.util.concurrent.ThreadLocalRandom;
+
 
 /** 
  * 
- * (Game: add three numbers) The program in Listing 3.1, AdditionQuiz.java, generates
- * two integers and prompts the user to enter the sum of these two integers.
- * Revise the program to generate three single-digit integers and prompt the user to
- * enter the sum of these three integers.
- * 				
- * 			
+ * (Game: addition quiz) Listing 3.3, SubtractionQuiz.java, randomly generates a
+ * subtraction question. Revise the program to randomly generate an addition question
+ * with two integers less than 100.
+ * 
  * */
-public class Exercise_3_2{
+public class Exercise_3_10{
 
 	/** Constructor not utilized */
-	public Exercise_3_2() {
-		
+	public Exercise_3_10() {
+
 	}//close constructor
 
 	/** Automatic generated main method */
 	public static void main(String[] args) {
 
-//		int number1 = ThreadLocalRandom.current().nextInt(100, 1000 + 1);
 
 		
 		Scanner input = new Scanner(System.in);
 		int number1 = (int) (Math.random()* 900 + 101);
 		int number2 = (int) (Math.random()* 900 + 101);
 		
-		System.out.print("\n\n\tSum two integers numbers");
+		System.out.print("\n\n\tSubtract two integers numbers");
 		System.out.print("\n\n\t");
 		System.out.print("\n\n\tFirst number is  --> " + number1);
 		System.out.print("\n\n\tFirst number is  --> " + number2);
@@ -38,13 +36,13 @@ public class Exercise_3_2{
 		int answer1 = input.nextInt();
 	
 		
-		if(answer1 == (number1 + number2)) {
+		if(answer1 == (number1 - number2)) {
 			int number3 = (int) (Math.random()* 900 + 101);
 			int number4 = (int) (Math.random()* 900 + 101);
 			int number5 = (int) (Math.random()* 900 + 101);
 			
 			
-			System.out.print("\n\n\tSum trhee integers numbers");
+			System.out.print("\n\n\tSubtract trhee integers numbers");
 			System.out.print("\n\n\t");
 			System.out.print("\n\n\tFirst number is  --> " + number3);
 			System.out.print("\n\n\tSecond number is  --> " + number4);
@@ -52,7 +50,7 @@ public class Exercise_3_2{
 			System.out.print("\n\n\t");
 			System.out.print("\n\n\tWhat is your answer ?  --> ");
 			int answer2 = input.nextInt();
-			if(answer2 == (number3 + number4 + number5)) {
+			if(answer2 == (number3 - number4 - number5)) {
 				System.out.print("\n\n\tCongratulations! You won!!!");
 			}else {
 				System.out.print("\n\n\tSORRY. Start the game again. YOU LOOSE!");
@@ -63,8 +61,11 @@ public class Exercise_3_2{
 		}
 		
 
+
 		input.close();
 
-	}//close main method
 
-}//close class Exercise_3_2
+
+}//close main method
+
+}//close class Exercise_3_10
---
+++ src/Chapter_3/Exercise_3_19.java
@@ -0,0 +1,9 @@
+package Chapter_3;
+
+public class Exercise_3_19 {
+
+	public Exercise_3_19() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
