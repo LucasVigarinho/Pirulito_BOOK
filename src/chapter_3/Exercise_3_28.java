cd5207ce6d628089a947e0fc5fddb7fd6295f519
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-09-20 19:50:51 +0100

    Commit including the solution of Exercise_3_28

--- src/chapter_3/Exercise_3_28.java
+++ src/chapter_3/Exercise_3_28.java
@@ -1,7 +1,7 @@
 package chapter_3;
 
 
-
+import java.util.Scanner;
 /**
  * 
  * (Geometry: two rectangles) Write a program that prompts the user to enter the
@@ -39,8 +39,48 @@ public class Exercise_3_28 {
 
 	/** creating the main method */
 	public static void main(String[] args) {
-	
+
+		Scanner input = new Scanner (System.in);
+
+		//Print out the message for the user insert coordinates coordinates of the first rectangle
+		System.out.print("\n\n\tEnter a point's x- and y- coordinates: ");
+		double x1 = input.nextDouble();
+		double y1 = input.nextDouble();
+		double width1 = input.nextDouble();
+		double height1 = input.nextDouble();
+		//Print out the message for the user insert coordinates of the second rectangle
+		System.out.print("\n\n\tEnter a point's x- and y- coordinates: ");
+		double x2 = input.nextDouble();
+		double y2 = input.nextDouble();
+		double width2 = input.nextDouble();
+		double height2 = input.nextDouble();
+
+		//calculation
+		double rec1 = (Math.pow(Math.pow(y2 - y1, 2), .05)) + height1 / 2;
+		double rec2 = (Math.pow(Math.pow(x2 - x1, 2), .05)) + width2 / 2;
+
+		//Determine whether the second rectangle is inside the first
+		if	((rec1 <= (height1 / 2)) && (rec2 <= (width1 / 2)) && ((height1 / 2 + height2 / 2) <= height1) &&	((width1 / 2 + width2 / 2) <= width1))
+
+				
+			System.out.println("\n\n\tRectangle 2 is inside Rectangle 1");
+
+		else if ((x1 + width1 / 2 > x2 - width2) ||  (y1 + height1 / 2 > y2 - height2))
+
+			System.out.println("\n\n\tRectangle 2 overlaps Rectangle 1");
+
+		else
+
+			System.out.println("\n\n\tRectangle 2 does not overlap Rectangle 1");
+
+		input.close();
 		
 	}//closing the main method
 
+
 }//closing class exercise_3_28
+//
+//((Math.pow(Math.pow(r2y - r1y, 2), .05) + r2Height / 2 <= r1Height / 2) && 
+//		(Math.pow(Math.pow(r2x - r1x, 2), .05) + r2Width / 2 <= r1Width / 2) &&
+//		(r1Height / 2 + r2Height / 2 <= r1Height) &&
+//		(r1Width / 2 + r2Width / 2 <= r1Width))
