99e54eaf8c7a0505afb96ee3828d274bc0b059fd
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-09-20 18:49:31 +0100

    including exercise_3_26

--- src/chapter_3/Exercise_3_26.java
+++ src/chapter_3/Exercise_3_26.java
@@ -27,7 +27,26 @@ public class Exercise_3_26 {
 	/** creating the main method */
 	public static void main(String[] args) {
 	
+		int x = 10;
+		int y = x % 5;
+		int z = x % 6;
 		
+		boolean ans = false;
+		
+		if(y == 0 && z == 0) {
+			ans = true;
+		}
+		System.out.print("\n\n\tIs 10 divisible by 5 and 6? " + ans);
+		
+		if(y == 0 || z == 0) {
+			ans = true;
+		}
+		System.out.print("\n\n\tIs 10 divisible by 5 or 6? " + ans);
+		
+		if(y == 0 ^ z == 0) {
+			ans = true;
+		}
+		System.out.print("\n\n\tIs 10 divisible by 5 or 6,  but not both? " + ans);
 	}//closing the main method
 
 }//closing class exercise_3_26
