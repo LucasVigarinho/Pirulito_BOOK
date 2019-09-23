64308fd1bb5e992a2fdf6c3d7e52b16c3d42d2f1
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-09-23 16:46:37 +0100

    Commit making questions ready to be answered - 3_21 to 3_34

---
+++ src/chapter_3/Exercise_3_33.java
@@ -0,0 +1,38 @@
+package chapter_3;
+
+
+
+/**
+ * 
+ * (Financial: compare costs) Suppose you shop for rice in two different packages.
+ * You would like to write a program to compare the cost. The program prompts the
+ * user to enter the weight and price of the each package and displays the one with
+ * the better price. Here is a sample run:
+ * 
+ * Enter weight and price for package 1: 50 24.59
+ * Enter weight and price for package 2: 25 11.99
+ * Package 2 has a better price.
+ * 
+ * 
+ * Enter weight and price for package 1: 50 25
+ * Enter weight and price for package 2: 25 12.5
+ * Two packages have the same price.
+ * 
+ * 
+ */
+
+/** Defining the main class*/
+public class Exercise_3_33 {
+
+	/** Defining the constructor of the class */
+	public Exercise_3_33() {
+
+	}//closing constructor
+
+	/** creating the main method */
+	public static void main(String[] args) {
+	
+		
+	}//closing the main method
+
+}//closing class exercise_3_29
---
+++ src/chapter_3/Exercise_3_34.java
@@ -0,0 +1,36 @@
+package chapter_3;
+
+
+
+/**
+ * 
+ *(Geometry: point on line segment) Programming Exercise 3.32 shows how to test
+ * whether a point is on an unbounded line. Revise Programming Exercise 3.32 to
+ * test whether a point is on a line segment. Write a program that prompts the user
+ * to enter the three points for p0, p1, and p2 and displays whether p2 is on the line
+ * segment from p0 to p1. Here are some sample runs:
+ * 
+ * 
+ * Enter three points for p0, p1, and p2: 1 1 2.5 2.5 1.5 1.5
+ * (1.5, 1.5) is on the line segment from (1.0, 1.0) to (2.5, 2.5)
+ * 
+ * Enter three points for p0, p1, and p2: 1 1 2 2 3.5 3.5
+ * (3.5, 3.5) is not on the line segment from (1.0, 1.0) to (2.0, 2.0)
+ * 
+ */
+
+/** Defining the main class*/
+public class Exercise_3_34 {
+
+	/** Defining the constructor of the class */
+	public Exercise_3_34() {
+
+	}//closing constructor
+
+	/** creating the main method */
+	public static void main(String[] args) {
+	
+		
+	}//closing the main method
+
+}//closing class exercise_3_29
