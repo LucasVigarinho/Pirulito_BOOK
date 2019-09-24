62efa2a7847f479c5788775ff2ebb2016dfd626b
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-09-24 19:43:11 +0100

    Commit including 3_30 solved

--- src/chapter_3/Exercise_3_30.java
+++ src/chapter_3/Exercise_3_30.java
@@ -1,6 +1,6 @@
 package chapter_3;
 
-
+import java.util.Scanner;
 
 /**
  * 
@@ -22,8 +22,48 @@ public class Exercise_3_30 {
 
 	/** creating the main method */
 	public static void main(String[] args) {
-	
 		
+		//Creating a new Scanner object
+		Scanner input = new Scanner(System.in);
+
+
+		//Asking to the user inform 
+		System.out.print("\n\tPlease, Enter the time zone offset to GMT:  \t--> ");
+
+
+		int timeZone = input.nextInt();
+
+		// milliseconds since midnight, Jan 1, 1970
+		long totalMilliseconds = System.currentTimeMillis();
+
+		long totalSeconds = totalMilliseconds/1000;
+		long totalMinutes = totalSeconds / 60;
+		long totalHours = totalMinutes / 60;
+
+		long totalSecondsNow = totalSeconds % 60;
+		long totalMinutesNow = totalMinutes % 60;
+		long totalHoursNow = totalHours % 24;
+
+		
+		//Confirming the value informed
+		System.out.println("\n\tThe number informed is \t\t--> " + timeZone);
+
+
+		if (totalHoursNow > 12) {
+			//Printing out the result of the transformation
+			System.out.println("\n\tThe current time is " + (totalHoursNow - 12) + ":" + totalMinutesNow + ":" + totalSecondsNow + " " + "PM" );
+
+		}else {
+			//Printing out the result of the transformation
+			System.out.println("\n\tThe current time is " + (totalHoursNow) + ":" + totalMinutesNow + ":" + totalSecondsNow + " " + "AM" );
+
+		}
+
+
+
+		input.close();
+
+
 	}//closing the main method
 
 }//closing class exercise_3_29
