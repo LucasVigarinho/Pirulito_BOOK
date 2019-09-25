56417c703a273b5352629d67318a113d0e688a8e
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-09-25 18:42:22 +0100

    Commit exercise 3_32 solved

--- src/chapter_3/Exercise_3_32.java
+++ src/chapter_3/Exercise_3_32.java
@@ -1,6 +1,7 @@
 package chapter_3;
 
-
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
 /**
  * 
@@ -55,8 +56,34 @@ public class Exercise_3_32 {
 
 	/** creating the main method */
 	public static void main(String[] args) {
-	
 		
+		//creating the objects to absorb the user informations and to print out in a good format
+		Scanner input = new Scanner (System.in);
+		DecimalFormat df = new DecimalFormat("####.0");
+		
+		//Printing out the message to be the user guide
+		System.out.print("\n\n\tEnter three points for p0, p1, and p2: ");
+		double x0 = input.nextDouble();
+		double y0 = input.nextDouble();
+		double x1 = input.nextDouble();
+		double y1 = input.nextDouble();
+		double x2 = input.nextDouble();
+		double y2 = input.nextDouble();
+		
+		
+		//calculation
+		double p2 = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
+                                                   
+		//if statement to control the right answer
+		if (p2 > 0) {
+			System.out.print("\n\n\t(" + x2 + ", " + y2 + " is on the left side of the line from(" + x0 + ", " + y0 + ") to (" +  x1 + ", " + y1  + ")");
+		}else if (p2 == 0) {
+			System.out.print("\n\n\t(" + x2 + ", " + y2 + " is on the line from (" + x0 + ", " + y0 + ") to (" +  x1 + ", " + y1  + ")");
+		}else {
+			System.out.print("\n\n\t(" + x2 + ", " + y2 + " is on the right side of the line from (" + x0 + ", " + y0 + ") to (" +  x1 + ", " + y1  + ")");
+		}
+		
+		input.close();
 	}//closing the main method
 
 }//closing class exercise_3_29
