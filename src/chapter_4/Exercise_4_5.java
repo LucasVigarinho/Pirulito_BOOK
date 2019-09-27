fb73027dcf8eb7b1f18e26ceb337c196c3f3927e
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-09-27 14:12:57 +0100

    commit exrcesise_4_5 solved

--- src/chapter_4/Exercise_4_5.java
+++ src/chapter_4/Exercise_4_5.java
@@ -1,5 +1,7 @@
 package chapter_4;
 
+
+
 /**
  * 
  * @author lucasmaximo
@@ -14,7 +16,7 @@ package chapter_4;
  * of a regular polygon is
  *  
  *  	         						  2
- *						 		     5 * S
+ *						 		     n * S
  * 							Area = ------------ 
  *								  4 * tan(PI/n)
  *
@@ -25,6 +27,7 @@ package chapter_4;
  *  Here, s is the length of a side. Write a program that prompts the user to enter the
  * number of sides and their length of a regular polygon and displays its area. Here is
  * a sample run:
+ * 
  * Enter the number of sides: 5
  * Enter the side: 6.5
  * The area of the polygon is 74.69017017488385
@@ -32,6 +35,8 @@ package chapter_4;
  */
 
 //necessary imports
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
 //creating a public class
 public class Exercise_4_5 {
@@ -43,7 +48,24 @@ public class Exercise_4_5 {
 
 	//Creating the main method
 	public static void main (String[] args) {
+		//Creating the necessary objects to absorb the user information and to format the print out 
+		Scanner input = new Scanner (System.in);
+		DecimalFormat df = new DecimalFormat("####.00");
 		
+		//print out one message to give the user chance to insert information
+		System.out.print("\n\n\tEnter the number of sides: ");
+		double n = input.nextDouble();
+		System.out.print("\n\n\tEnter the side: ");
+		double side = input.nextDouble();
+		
+		//calculate the area of the hexagon utilizing the formula
+		double area = (n * Math.pow(side, 2)) / (4 * Math.tan(Math.PI/n)); 
+		
+		//printing out the answer 
+		System.out.print("\n\n\tThe area of the polygon is  " + df.format(area));
+		
+		//closing the object
+		input.close();
 	}//closing the main method
 	
 	
