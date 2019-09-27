11e628ff98479302fa314b3740b70e5ead33c9c1
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-09-27 15:02:39 +0100

    commit exercises_4_6 and 4_7 solved

--- src/chapter_4/Exercise_4_6.java
+++ src/chapter_4/Exercise_4_6.java
@@ -27,6 +27,7 @@ package chapter_4;
  */
 
 //necessary imports
+import java.text.DecimalFormat;
 
 //creating a public class
 public class Exercise_4_6 {
@@ -39,6 +40,37 @@ public class Exercise_4_6 {
 	//Creating the main method
 	public static void main (String[] args) {
 		
+		DecimalFormat df = new DecimalFormat("####,00");
+		
+		double r = 40;
+		
+		// Creating random angles in radians between 0 and 2PI
+		double a1 = ( Math.random () * ( Math.PI * 2 ));
+		double a2 = ( Math.random () * ( Math.PI * 2 ));
+		double a3 = ( Math.random () * ( Math.PI * 2 ));
+
+		// Defining x and y form the angles above
+		double x1 = r * Math.cos ( a1 );
+		double y1 = r * Math.sin ( a1 );
+		double x2 = r * Math.cos ( a2 );
+		double y2 = r * Math.sin ( a2 );
+		double x3 = r * Math.cos ( a3 );
+		double y3 = r * Math.sin ( a3 );
+
+		// Compute the three sides
+		double a = Math.pow (( Math.pow(( x2 - x3 ), 2 ) + Math.pow(( y2 - y3 ), 2 )), 0.5 );
+		double b = Math.pow (( Math.pow(( x1 - x3 ), 2 ) + Math.pow(( y1 - y3 ), 2 )), 0.5 );
+		double c = Math.pow (( Math.pow(( x1 - x2 ), 2 ) + Math.pow(( y1 - y2 ), 2 )), 0.5 );
+
+		// Compute three angles following the formula
+		double aA = Math.toDegrees (Math.acos((( a * a ) - ( b * b ) - ( c * c )) / ( -2 * b * c )));
+		double aB = Math.toDegrees (Math.acos((( b * b ) - ( a * a ) - ( c * c )) / ( -2 * a * c )));
+		double aC = Math.toDegrees (Math.acos((( c * c ) - ( b * b ) - ( a * a )) / ( -2 * a * b )));
+
+		// Display results
+		System.out.println("\n\n\tThe three angles are " + df.format(aA) + " " + df.format(aB)+ " " + df.format(aC) );	
+
+	
 	}//closing the main method
 	
 	
--- src/chapter_4/Exercise_4_7.java
+++ src/chapter_4/Exercise_4_7.java
@@ -1,5 +1,8 @@
 package chapter_4;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 /**
  * 
  * @author lucasmaximo
@@ -39,9 +42,47 @@ public class Exercise_4_7 {
 
 	//Creating the main method
 	public static void main (String[] args) {
+
+		//Creating the necessary objects to absorb the user information and to format the print out 
+		Scanner input = new Scanner (System.in);
+		DecimalFormat df = new DecimalFormat("####.0000");
+
+		//print out one message to give the user chance to insert information
+		System.out.print("\n\n\tEnter the radius of the bounding circle: ");
+		double r = input.nextDouble();
+
+		// Creating random angles in radians between 0 and 2PI
+		double a1 = ( Math.random () * ( Math.PI * 2 ));
+		double a2 = ( Math.random () * ( Math.PI * 2 ));
+		double a3 = ( Math.random () * ( Math.PI * 2 ));
+		double a4 = ( Math.random () * ( Math.PI * 2 ));
+		double a5 = ( Math.random () * ( Math.PI * 2 ));
+
+		// Defining x and y form the angles above
+		double x1 = r * Math.cos ( a1 );
+		double y1 = r * Math.sin ( a1 );
+		double x2 = r * Math.cos ( a2 );
+		double y2 = r * Math.sin ( a2 );
+		double x3 = r * Math.cos ( a3 );
+		double y3 = r * Math.sin ( a3 );
+		double x4 = r * Math.cos ( a4 );
+		double y4 = r * Math.sin ( a4 );
+		double x5 = r * Math.cos ( a5 );
+		double y5 = r * Math.sin ( a5 );
+
+		//print out the five si
+		System.out.println("\n\n\tThe coordinates of five points on the pentagon are \n\n" );	
+		System.out.println("\t (\t" + df.format(x1) + "\t\t, \t" + df.format(y1) + "\t )" );	
+		System.out.println("\t (\t" + df.format(x2) + "\t\t, \t" + df.format(y2) + "\t )" );	
+		System.out.println("\t (\t" + df.format(x3) + "\t\t, \t" + df.format(y3) + "\t )" );	
+		System.out.println("\t (\t" + df.format(x4) + "\t\t, \t" + df.format(y4) + "\t )" );	
+		System.out.println("\t (\t" + df.format(x5) + "\t\t, \t" + df.format(y5) + "\t )" );	
+
+		//closing the object scanner used to absorbs the user input
+		input.close();
 		
 	}//closing the main method
-	
-	
+
+
 }//closing the public class Exercise_4_7
 
