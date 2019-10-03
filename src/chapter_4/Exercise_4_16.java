c6ee045efd4342949d941f67f10b9def79b124a4
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-03 20:04:11 +0100

    commit exercise_4_15 solved

--- src/chapter_4/Exercise_4_16.java
+++ src/chapter_4/Exercise_4_16.java
@@ -1,7 +1,13 @@
 package chapter_4;
 
+import java.util.Random;
+
 
 /**    ************* Description of the Exercise_4_16 *************
+ * 
+ * 
+ * (Random character) Write a program that displays a random uppercase letter
+ * using the Math.random() method.
  * 
  * 
  *  @author lucasmaximo
@@ -10,10 +16,23 @@ package chapter_4;
 
 /** Necessary imports */
 
+
 /** Creating the class named Exercise_4_16 */
 public class Exercise_4_16 {
 
 	/** Creating the main method */
-	
+	public static void main(String[] args) {
+		
+		//create the object to absorb the user insertion
+		Random r = new Random();
+		
+		char c = (char)(r.nextInt(26) + 'a');
+		
+		c = Character.toUpperCase(c);
+
+		System.out.print("\n\n\t" + c);
+		 
+		
+	}//closing the main method
 	
 }//closing the class_4.16
