42a68f2f0499ee68b976361674e82ff9ee2e60ac
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-05 19:44:09 +0100

    commit the exercise 4_26 solved, the last exercise of Chapter_4

--- src/chapter_4/Exercise_4_26.java
+++ src/chapter_4/Exercise_4_26.java
@@ -13,14 +13,48 @@ package chapter_4;
  * */
 
 /** Necessary imports */
-
+import java.util.Scanner;
 
 /** Creating the class named Exercise_4_21 */
 public class Exercise_4_26 {
 
 	/** Creating the main method */
-	public static void main(String[] arg) {
+	public static void main(String[] args) {
+		// Create the object through Scanner to absorb the user inserction
+		Scanner input = new Scanner(System.in);
 
+		// printing out the message to the user insert the amount
+		System.out.print("Enter an amount, for example 11.56: ");
+		String amount = input.nextLine();
+
+		// using the substring 0 determined by the indexOf as a '.' the limit
+		String numberOfOneDollars = amount.substring(0, amount.indexOf('.'));
+
+		// transforming the cents part as a Double to a integer
+		int cents = Integer.parseInt(amount.substring(amount.indexOf('.') + 1));
+
+		// Analyzed and find out the number of quarters
+		int numberOfQuarters = cents / 25;
+		cents %= 25;
+
+		// number of dimes
+		int numberOfDimes = cents / 10;
+		cents %= 10;
+
+		// number of nickels
+		int numberOfNickels = cents / 5;
+		cents %= 5;
+
+		// Print out the correct answer
+		System.out.println("\n\n\tYour amount " + amount + " consists of\n " 
+			+ "\t\t\t\t\t" + numberOfOneDollars + " dollars\n " 
+			+ "\t\t\t\t\t" +numberOfQuarters + " quarters\n "
+			+ "\t\t\t\t\t" +numberOfDimes + " dimes\n "
+			+ "\t\t\t\t\t" +numberOfNickels + " nickels\n "
+			+ "\t\t\t\t\t" +cents + " pennies\n "
+			);
+	
+		input.close();
 
 
 	}//closing the main method
