20a0f00b1ca911398ec78aeee2043ff99122bb28
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-10 22:27:17 +0100

    commit exercise_5_12 solved

--- src/chapter_5/Exercise_5_12.java
+++ src/chapter_5/Exercise_5_12.java
@@ -3,7 +3,7 @@ package chapter_5;
 
 /**    ************* Description of the Exercise_5_12 *************
  * 
- * 		(Find the smallest n such that n2 7 12,000) Use a while loop to find the smallest
+ * 		(Find the smallest n such that n2 > 12,000) Use a while loop to find the smallest
  * 		integer n such that n2 is greater than 12,000.
  * 
  * 
@@ -22,8 +22,15 @@ public class Exercise_5_12 {
 		
 		
 		//create the necessary variables
-	
+		int n = 0;
 		
+		//creating a while loop in order test the number
+		while(Math.pow(n,2) < 12000) {
+			n++;
+			
+		}//closing while loop
+		
+		System.out.print("\n\n\t" + n);
 	}//closing the main method
 
 
