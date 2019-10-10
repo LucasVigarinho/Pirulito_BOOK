8a23df91606efe4792b34a1d7c0d49e36783320d
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-10 22:29:13 +0100

    commit exercise_5_13 solved

--- src/chapter_5/Exercise_5_13.java
+++ src/chapter_5/Exercise_5_13.java
@@ -3,7 +3,7 @@ package chapter_5;
 
 /**    ************* Description of the Exercise_5_13 *************
  * 
- * 		(Find the largest n such that n3 6 12,000) Use a while loop to find the largest
+ * 		(Find the largest n such that n3 < 12,000) Use a while loop to find the largest
  * 		integer n such that n3 is less than 12,000.
  * 
  * 
@@ -23,9 +23,17 @@ public class Exercise_5_13 {
 		
 		
 		//create the necessary variables
-	
+		int n = 0;
 		
+		//creating a while loop in order test the number
+		while(Math.pow(n,3) < 12000) {
+			n++;
+			
+		}//closing while loop
+		
+		System.out.print("\n\n\t" + n);
 	}//closing the main method
+
 
 
 }//closing the class_5_13
