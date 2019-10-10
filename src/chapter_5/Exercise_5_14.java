a6445456ce9aecd22d849f9f77b82686e46f549e
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-10 23:13:48 +0100

    commit exercise_5_14 solved

--- src/chapter_5/Exercise_5_14.java
+++ src/chapter_5/Exercise_5_14.java
@@ -17,18 +17,42 @@ package chapter_5;
  * */
 
 /** Necessary imports */
-
+import java.util.Scanner;
 
 /** Creating the class named Exercise_5_14 */
 public class Exercise_5_14 {
 
 	/** Creating the main method */
 	public static void main(String[] arg) {
+
+
+
+
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+
+		// Prompt the user to enter two integers
+		System.out.print("Enter first integer: ");
+		int n1 = input.nextInt();
+		System.out.print("Enter second integer: ");
+		int n2 = input.nextInt();
+
+		int gcd = 1; // Initial gcd is 1
+		int k = 2; // Possible gcd
 		
-		
-		//create the necessary variables
-	
-		
+		//create a do loop
+		do {
+			if (n1 % k == 0 && n2 % k == 0)
+				gcd = k; // Update gcd
+			k++;
+		}while (n1 >= k && n2 >= k) ;
+
+		System.out.println("The greatest common divisor for " + n1 +
+				" and " + n2 + " is " + gcd);
+
+
+		input.close();
+		 
 	}//closing the main method
 
 
