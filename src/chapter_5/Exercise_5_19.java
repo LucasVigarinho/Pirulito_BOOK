4c1ce0a4abbfd34c653747683c469830974a48ba
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-11 14:48:11 +0100

    commit exercise_5_20 solved

--- src/chapter_5/Exercise_5_19.java
+++ src/chapter_5/Exercise_5_19.java
@@ -1,5 +1,6 @@
 package chapter_5;
 
+import java.util.Scanner;
 
 /**    ************* Description of the Exercise_5_19 *************
  * 
@@ -28,12 +29,71 @@ public class Exercise_5_19 {
 
 	/** Creating the main method */
 	public static void main(String[] arg) {
-		
-		
-		//create the necessary variables
-	
-		
-	}//closing the main method
+		Scanner s = new Scanner(System.in);
+		// Get user input = number of lines to print in a pyramid
+		System.out.println("Enter a number between 1 and 15: ");
+		int numLines = s.nextInt();
+		//boolean statement with do loop and if statement to make sure that the limit between 1 and 15 is being followed by the user
+		boolean valid = false;
+		//create a do loop to keep the user inputing information in case that it had insert a different number out of the range
+		do {
+			if (numLines < 1 || numLines > 15) {
+				System.out.println("Number incorrect, enter again a number between 1 and 15: ");
+				numLines = s.nextInt();
+				valid = false;
+			} else {
+				valid = true;
+				printPyramid(numLines);
+				//closing Scanner
+				s.close();
+			}
+		} while (!valid);
+	}
+
+	// creating the private class to make the pyramid
+	private static void printPyramid(int numLines) {
+		int row = 1;
+		int col = 1;
+		int counter = 1;
+		// creating the loop rows 
+		while (row <= numLines) {
+			// loop tabulation before print digits
+			while (col <= (numLines - row)) {
+				System.out.print(" \t");
+				col++;
+			}
+			if (row == 1) {
+				System.out.print(row + "\t");
+			} else {
+				// loop first half pyramid
+				int digit = 1;
+				while (counter <= row) {
+					digit = digit * 2;
+					if (counter == 1) {
+						System.out.print(counter + "\t");
+					} else {
+						System.out.print(digit / 2 + "\t");
+					}
+					counter++;
+				}
+				// print second half pyramid
+				counter--;
+				digit = digit / 2;
+				while (counter > 1) {
+					counter--;
+					digit = digit / 2;
+					System.out.print(digit + "\t");
+				}
+			}
+			row++;
+			col = 1;
+			counter = 1;
+			// jump one line
+			System.out.println();
+
+		}
+	}
+
 
 
 }//closing the class_5_19
--- src/chapter_5/Exercise_5_20.java
+++ src/chapter_5/Exercise_5_20.java
@@ -21,11 +21,38 @@ public class Exercise_5_20 {
 
 	/** Creating the main method */
 	public static void main(String[] arg) {
-		
-		
-		//create the necessary variables
+
+		int count=0;
+		System.out.print("\n\n\t ");
 	
-		
+		//create the necessary variables
+		for (int i = 2; i <= 1000; i++) {
+			boolean prime = true;
+
+			//open for loop in order to check if is or not prime
+			for (int a = 2; a <= i/2; a++) {
+				//create an if statement to test the correct number and print out
+				if (i % a == 0 ) {
+					prime = false;
+					break;
+				}//closing if statement
+			}//closing for loop
+			
+
+			//open if statement 
+			if(prime) {
+				System.out.print(i + "\t");
+				count++;
+				//open if statement to jump to the next line
+				if(count == 8) {
+					count = 0;
+					System.out.println();
+					System.out.print("\t ");
+				}//closing if statement jumping line
+			}//closing if statement printing prime
+			
+		}//closing for loop of a test
+
 	}//closing the main method
 
 
