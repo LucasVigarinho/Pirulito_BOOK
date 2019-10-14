b7db8fe6a3138bcc730f7a8d851a8fe5c702039e
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-14 16:43:47 +0100

    commit exercise 5_24 sp;ved

--- src/chapter_5/Exercise_5_24.java
+++ src/chapter_5/Exercise_5_24.java
@@ -23,7 +23,14 @@ public class Exercise_5_24 {
 
 	
 		//create the necessary variables
-
+		double count = 0;
+		
+		//create a for loop to start the sum
+		for (double i = 3.0; i <= 99; i += 2) {
+			count += (i-2)/i;
+		}//closing for loop
+		
+		System.out.print("\n\n\tThe result is -> " + count);
 	}//closing the main method
 
 
