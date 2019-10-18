928ae308fa6277e1f89e3de49e56e2ab475f03f0
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-18 16:47:27 +0100

    commit exercise 5_33 solved

--- src/chapter_5/Exercise_5_33.java
+++ src/chapter_5/Exercise_5_33.java
@@ -24,9 +24,32 @@ public class Exercise_5_33 {
 	public static void main(String[] arg) {
 
 		//create the necessary objects
-	
-		//calculation
+		System.out.print("\n\n\t There are four perfect numbers less than 10,000 ");
 		
+		int test = 0;
+
+		//calculation
+		//create a for loop to test all the numbers between 1 and 10000
+		for (int x = 1; x <=10000; x++) {
+			
+			//create a for loop to be able to sum the number divisors by the number x from the main for loop
+			for (int y = 1; y < x; y++ ) {
+				
+				//create the if statement in order to summarize the divisors numbers to test in the follow if statement
+				if (x % y == 0) {
+					test += y;
+				}//closing if statement
+				
+			}//closing nested for loop
+			
+			//if statement in order to print the number which match the necessary status
+			if(test == x) {
+				System.out.print("\n\t" + test);
+			}//closing if statement
+			
+			//Start from zero the test variable, to be able to calculate the next number
+			test = 0;
+		}//closing main for loop
 		
 		
 	}//closing the main method
