751497a89e9364af0b4e72d2f4c4266e51feaac9
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-23 11:25:31 +0100

    commit @LucasVigarinho exercise_5_37 solved

--- src/chapter_5/Exercise_5_37.java
+++ src/chapter_5/Exercise_5_37.java
@@ -13,7 +13,7 @@ package chapter_5;
 
 /** Necessary imports */
 
-
+import java.util.Scanner;
 
 /** Creating the class named Exercise_5_37 */
 public class Exercise_5_37 {
@@ -21,12 +21,27 @@ public class Exercise_5_37 {
 	/** Creating the main method */
 	public static void main(String[] arg) {
 
+		Scanner input = new Scanner(System.in);
 		//create the necessary objects
-	
+		System.out.print("\n\n\tEnter the decimal integer -> ");
+		int decimalInteger = input.nextInt();
+
 		//calculation
-		
-		
-		
+		// create the string in order to print the binary number 
+		String binary = "";	
+		//create the for loop in order to divide the number per 2 (decimal) in order to find each space
+		for (int i = decimalInteger; i > 0; i /= 2) {
+			//complete the string from right to the left
+			binary = (i % 2) + binary; 
+		}//closing for loop
+
+		// Print out the correct answer
+		System.out.println(
+				"\n\n\tThe binary value of the decimial \"" + decimalInteger + "\" is: " + binary);
+		//closing input object
+		input.close();
+
+
 	}//closing the main method
 
 
