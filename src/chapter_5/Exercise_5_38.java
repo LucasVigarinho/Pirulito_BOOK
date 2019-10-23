6426c2ef2063a2eb6aa59b31a23fe1d0054c8b21
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-23 11:44:14 +0100

    commit @LucasVigarinho exercise_5_38 solved

--- src/chapter_5/Exercise_5_38.java
+++ src/chapter_5/Exercise_5_38.java
@@ -1,5 +1,6 @@
 package chapter_5;
 
+import java.util.Scanner;
 
 /**    ************* Description of the Exercise_5_38 *************
  * 
@@ -21,12 +22,29 @@ public class Exercise_5_38 {
 	/** Creating the main method */
 	public static void main(String[] arg) {
 
+		Scanner input = new Scanner(System.in);
 		//create the necessary objects
-	
+		System.out.print("\n\n\tEnter the decimal integer -> ");
+		int decimalInteger = input.nextInt();
+
 		//calculation
-		
-		
-		
+		// create the string in order to print the octal number 
+		String octal = "";	
+		//create the for loop in order to divide the number per 2 (decimal) in order to find each space
+		for (int i = decimalInteger; i > 0; i /= 8) {
+			
+			//complete the string from right to the left
+			
+			octal = (i % 8) + octal; 
+		}//closing for loop
+
+		// Print out the correct answer
+		System.out.println(
+				"\n\n\tThe binary value of the decimial \"" + decimalInteger + "\" is: " + octal);
+		//closing input object
+		input.close();
+
+
 	}//closing the main method
 
 
