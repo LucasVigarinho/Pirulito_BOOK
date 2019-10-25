96a824b7df03344d48dc7e82cbf7579da9ac9130
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-25 19:55:50 +0100

    commit @LucasVigarinho exercise_5_40 solved

--- src/chapter_5/Exercise_5_40.java
+++ src/chapter_5/Exercise_5_40.java
@@ -1,6 +1,7 @@
 package chapter_5;
 
 
+
 /**    ************* Description of the Exercise_5_40 *************
  * 
  * 
@@ -12,20 +13,32 @@ package chapter_5;
  * */
 
 /** Necessary imports */
-
-
+import java.util.Random;
 
 /** Creating the class named Exercise_5_40 */
 public class Exercise_5_40 {
-
+	
 	/** Creating the main method */
 	public static void main(String[] arg) {
 
 		//create the necessary objects
-	
+		Random random = new Random();
+		int coin = (int) random.nextInt(2);
+		
+		int coinFace = 0;
+		int coinTail = 0;
+		
 		//calculation
-		
-		
+		for(int i = 0; i < 1000000; i++) {
+			coin = (int) random.nextInt(2);
+			if(coin == 1) {
+				coinFace+=1;
+			}else {
+				coinTail+=1;
+			}
+		}
+		System.out.print("\n\n\t Face -> " + coinFace);
+		System.out.print("\n\t Tail -> " + coinTail);
 		
 	}//closing the main method
 
