0007be97a5886a1e878226eee73ff97df1088652
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-31 22:38:35 +0000

    commit @LucasVigarinho exercise_6_01

--- src/chapter_6/Exercise_6_01.java
+++ src/chapter_6/Exercise_6_01.java
@@ -26,22 +26,26 @@ public class Exercise_6_01{
 	public static void main(String[] arg) {
 
 		//create the necessary objects
-		
+		Scanner input = new Scanner(System.in);
 
-		//print out the message to be the user guide in the interaction
-		
-		//Create a variable to absorb the user insertion
-		
-
-		//create a variable necessary to count
-		
-		
+		System.out.print("\n\t");
 		//create a for loop in order to test all the letters
-		//closing for loop
+		for(int i = 1; i <= 100; i++) {
 		
+			System.out.print((i%10==0)?"\n\t":getPentagonalNumber(i) + "\t");
+
+		}//close for loop
 		//print out the messages 
 		
+		input.close();
 	}//closing the main method
 
+	/** Method defined on the instructions */
+	public static int getPentagonalNumber(int n) {
+		n = (n * (3*n-1))/2;
+		
+		return n;
+		
+	}//closing method getPentagonal
 
 }//closing the class_6_01
