8377135b1ea2a106bfbb6b2dc58ddbe22b85b58f
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-10-31 17:01:16 +0000

    commit @LucasVigarinho exercise_5_48 solved

--- src/chapter_5/Exercise_5_47.java
+++ src/chapter_5/Exercise_5_47.java
@@ -5,7 +5,7 @@ package chapter_5;
 /**    ************* Description of the Exercise_5_47 *************
  * 
  * 
- * 		(Business: check ISBN-13) ISBN-13 is a new standard for indentifying books. It
+ * 		(Business: check ISBN-13) ISBN-13 is a new standard for identifying books. It
  * 		uses 13 digits d1d2d3d4d5d6d7d8d9d10d11d12d13. The last digit d13 is a checksum,
  * 		which is calculated from the other digits using the following formula:
  * 		10 - (d1 + 3d2 + d3 + 3d4 + d5 + 3d6 + d7 + 3d8 + d9 + 3d10 + d11 + 3d12)%10
@@ -27,19 +27,52 @@ package chapter_5;
  * */
 
 /** Necessary imports */
-
+import java.util.Scanner;
 
 /** Creating the class named Exercise_5_47 */
 public class Exercise_5_47 {
-	
+
 	/** Creating the main method */
 	public static void main(String[] arg) {
 
 		//create the necessary objects
-		
-		
-		//calculation
-		
+		Scanner input = new Scanner(System.in);
+
+		//create the necessary variable
+		int numberChar = 0, summ = 0;
+		boolean count = false;
+
+		//print out the user interaction possibility
+		System.out.print("\n\n\tEnter the first 12 digits of an ISBN-13 as a string: ");
+		String first12 = input.nextLine();//absorb the user input
+
+		switch(first12.length()) {
+		case(12):
+			//create a for loop in order to make possible the calculation
+			for(int i = 0; i < first12.length(); i++) {
+				if(count == true) {
+					numberChar = (int) Integer.parseInt(String.valueOf(first12.charAt(i)));
+					summ += numberChar*3;
+					count = false;
+				}else {
+					numberChar = (int) Integer.parseInt(String.valueOf(first12.charAt(i)));
+					summ += numberChar;
+					count = true;
+				}
+
+			}//closing for loop 
+		/** Complete the calculation in order to test the final number */
+		summ = 10 - (summ%10);
+
+		/** Print out the answer message for the two possibilities */
+		System.out.print((summ == 10)?("\n\n\tThe ISBN-13 number is" + first12 + "0" ):
+						              ("\n\n\tThe ISBN-13 number is" + first12 + summ)); 
+		break;
+		default:
+			System.out.print("\n\n\t" + first12 + "is an invalid input");
+		}//closing switch statement
+
+		input.close();
 	}//closing the main method
 
 
