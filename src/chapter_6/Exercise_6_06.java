95cbc6c0d8319d1f478516ce3315e7720c99451b
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-11-01 12:30:55 +0000

    commit @LucasVigarinho exercise_6_06 solved

--- src/chapter_6/Exercise_6_06.java
+++ src/chapter_6/Exercise_6_06.java
@@ -27,22 +27,37 @@ public class Exercise_6_06{
 	public static void main(String[] arg) {
 
 		//create the necessary objects
-		
+		Scanner input = new Scanner(System.in);
 
 		//print out the message to be the user guide in the interaction
-		
+		System.out.print("\n\n\tType the size of the patter to be printed -> ");
 		//Create a variable to absorb the user insertion
-		
+		int number = input.nextInt();
 
-		//create a variable necessary to count
-		
-		
-		//create a for loop in order to test all the letters
-		//closing for loop
-		
-		//print out the messages 
-		
+		//call the method to create the patter
+		displayPattern(number);
+
 	}//closing the main method
 
+	/** Create the pattern's method following the instructions */
+	public static void displayPattern(int n) {
+
+		//create a for loop responsible for the columns 
+		for(int i = 1; i <= n; i++) {
+			//Print out the visual position
+			System.out.print("\n\t\t\t\t\t\t\t");
+
+			//create a nested for loop in order to print the lines
+			for (int j = n; j >= 1; j--) {
+				//create a if statement in order to print the spaces or the number in order to create the pattern
+				System.out.print((j > i)?"  ":j + " ");
+
+			}//closing the nested for loop (lines)
+
+		}//closing the main for loop responsible by the columns
+		//print out the messages 
+
+
+	}//closing method pattern
 
 }//closing the class_6_06
