40283e4bf5cf89bc7eeddb90be91df672c50b721
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-11-05 20:10:30 +0000

    commit @LucasVigarinho adjusts

--- src/chapter_6/Exercise_6_15.java
+++ src/chapter_6/Exercise_6_15.java
@@ -10,30 +10,34 @@ package chapter_6;
  * 		Use this method to write a program that prints a tax table for taxable income from
  * 		$50,000 to $60,000 with intervals of $50 for all the following statuses:
  * 
- * 		Taxable
- * 		Income
- * 		Single Married Joint
- * 		or Qualifying
- * 		Widow(er)
- * 		Married
- * 		Separate
- * 		Head of
- * 		a House
- * 		50000 8688 6665 8688 7353
- * 		50050 8700 6673 8700 7365
+ * 		Taxable		Single 		Married Joint		Married			Head of
+ * 		Income					or Qualifying		Separate		a House
+ * 								Widow(er)
+ * 		--------------------------------------------------------------------
+ * 		50000 		8688 		6665 				8688 			7353
+ * 		50050 		8700 		6673 				8700 			7365
  * 		...
- * 		59950 11175 8158 11175 9840
- * 		60000 11188 8165 11188 9853
- * 		Enter n: 3
- * 		0 1 0
- * 		0 0 0
- * 		1 1 1
- * 		Hint: round the tax into integers using Math.round (i.e., Math
- * 		.round(computeTax(status, taxableIncome)).
+ * 		59950 		11175 		8158 				11175 			9840
+ * 		60000 		11188 		8165 				11188 			9853
+ * 
+ *	 	Hint: round the tax into integers using Math.round (i.e., Math
+ *	 	.round(computeTax(status, taxableIncome)).
  * 
  * @author lucasmaximo
  *
  */
+
+/** Create a exercise_6_15 class */
 public class Exercise_6_15 {
 
-}
+	/** Create the main method */
+	public static void main (String[] main) {
+		
+	}//closing main method
+	
+	/** Create a method following the instructions */
+	public static double computeTax(int status, double taxableIncome) {
+		
+		return 0;
+	}//close method
+}//close class
--- src/chapter_6/Exercise_6_16.java
+++ src/chapter_6/Exercise_6_16.java
@@ -8,9 +8,11 @@ package chapter_6;
  * 		public static int numberOfDaysInAYear(int year)
  * 
  * 		Write a test program that displays the number of days in year from 2000 to 2020.
+ * 
  * @author lucasmaximo
  *
  */
+/** Create a exercise_6_16 class */
 public class Exercise_6_16 {
 
-}
+}//close class_6_16
