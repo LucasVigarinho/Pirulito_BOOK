2a2b1a5c4b677861baa13659c963ce10810afdae
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-11-05 10:42:08 +0000

    commit @LucasVigarinho exercise_6_21 solved

--- src/chapter_6/Exercise_6_13.java
+++ src/chapter_6/Exercise_6_13.java
@@ -3,9 +3,11 @@ package chapter_6;
 /**
  * 
  * 		(Sum series) Write a method to compute the following series:
- * 		
- * 		m(i) =
  * 	
+ * 		
+ * 		m(i) = 1/2 + 2/3 + ... + i / (i+1)
+ * 	
+ * 
  * 		Write a test program that displays the following table:
  * 		i 		m(i)
  * 		1 		0.5000
@@ -17,6 +19,32 @@ package chapter_6;
  * @author lucasmaximo
  *
  */
+
+import java.text.DecimalFormat;;
+
+/** Created the class exercise 6 13 **/
 public class Exercise_6_13 {
 
-}
+	/** Creating a main class */
+	public static void main (String[] args) {
+
+		/** Created an object */
+		DecimalFormat df0 = new DecimalFormat("###");
+		DecimalFormat df4 = new DecimalFormat("###.0000");
+
+		/** Print out a message to display the table */
+		System.out.print("\n\n\tWrite a test program that displays the following table:");
+
+		System.out.print("\n\n\ti\tm(i)");
+
+		double sum = 0;
+
+		/** Create a for loop in order to print all the answers of the table */
+		for (double i = 1.0; i <= 20; i++) {
+			System.out.print("\n\n\t" + df0.format(i) + "\t" + df4.format(sum += i / (i + 1)));
+
+		}//closing for loop 
+
+	}//closing the main method
+
+}//closing the class
--- src/chapter_6/Exercise_6_21.java
+++ src/chapter_6/Exercise_6_21.java
@@ -1,5 +1,11 @@
 package chapter_6;
+/**
+ * 
+ * @author lucasmaximo
+ *
+ */
 
+//creating the class
 public class Exercise_6_21 {
 
-}
+}//closing the class
