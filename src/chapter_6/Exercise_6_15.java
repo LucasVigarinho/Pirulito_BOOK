823cbefd43bc3f8ec85cf4b9df01b3e07e8bc181
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-11-07 19:40:42 +0000

    commit @LucasVigarinho exercise_6_16

--- src/chapter_6/Exercise_6_15.java
+++ src/chapter_6/Exercise_6_15.java
@@ -61,70 +61,70 @@ public class Exercise_6_15 {
 		taxRate,		// Tax rate 
 		incomeTaxed, 	// Taxed income
 		incomeUntaxed, 	// Untaxed income
-		taxAt15Pecent, 	// Minimum marginal amount of 15% tax bracket
-		taxAt25Pecent, 	// Minimum marginal amount of 25% tax bracket
-		taxAt28Pecent, 	// Minimum marginal amount of 28% tax bracket
-		taxAt33Pecent, 	// Minimum marginal amount of 33% tax bracket
-		taxAt35Pecent;	// Minimum marginal amount of 35% tax bracket
+		taxAt15Percent, 	// Minimum marginal amount of 15% tax bracket
+		taxAt25Percent, 	// Minimum marginal amount of 25% tax bracket
+		taxAt28Percent, 	// Minimum marginal amount of 28% tax bracket
+		taxAt33Percent, 	// Minimum marginal amount of 33% tax bracket
+		taxAt35Percent;	// Minimum marginal amount of 35% tax bracket
 
 		// Initialize variables to 0
-		taxAt15Pecent = taxAt25Pecent = taxAt28Pecent = taxAt33Pecent = 
-				taxAt35Pecent = incomeUntaxed = taxRate = tax = 0.0;
+		taxAt15Percent = taxAt25Percent = taxAt28Percent = taxAt33Percent = 
+				taxAt35Percent = incomeUntaxed = taxRate = tax = 0.0;
 
 		switch (status) { // Get minimum taxableIncome for tax brackets  
 		case 0 : // Single
-			taxAt15Pecent = 8351; 
-			taxAt25Pecent = 33951; 
-			taxAt28Pecent = 82251; 
-			taxAt33Pecent = 171551; 
-			taxAt35Pecent = 372951; break;
+			taxAt15Percent = 8351; 
+			taxAt25Percent = 33951; 
+			taxAt28Percent = 82251; 
+			taxAt33Percent = 171551; 
+			taxAt35Percent = 372951; break;
 
 		case 1 : // Married Joint or Qualifying Widow(er)
-			taxAt15Pecent = 16701; 
-			taxAt25Pecent = 67901; 
-			taxAt28Pecent = 137051; 
-			taxAt33Pecent = 208851; 
-			taxAt35Pecent = 372951; break;
+			taxAt15Percent = 16701; 
+			taxAt25Percent = 67901; 
+			taxAt28Percent = 137051; 
+			taxAt33Percent = 208851; 
+			taxAt35Percent = 372951; break;
 
 		case 2 : // Married Filing Separately
-			taxAt15Pecent = 8351; 
-			taxAt25Pecent = 33951; 
-			taxAt28Pecent = 68526; 
-			taxAt33Pecent = 104426; 
-			taxAt35Pecent = 186476; break;
+			taxAt15Percent = 8351; 
+			taxAt25Percent = 33951; 
+			taxAt28Percent = 68526; 
+			taxAt33Percent = 104426; 
+			taxAt35Percent = 186476; break;
 
 		case 3 : // Head of house
-			taxAt15Pecent = 11951; 
-			taxAt25Pecent = 45501; 
-			taxAt28Pecent = 117451; 
-			taxAt33Pecent = 190201; 
-			taxAt35Pecent = 372951;
+			taxAt15Percent = 11951; 
+			taxAt25Percent = 45501; 
+			taxAt28Percent = 117451; 
+			taxAt33Percent = 190201; 
+			taxAt35Percent = 372951;
 		}
 
 		// Compute tax
-		while (taxableIncome >= taxAt15Pecent) {
+		while (taxableIncome >= taxAt15Percent) {
 
 			// Compute tax rate and untaxed income
-			if (taxableIncome >= taxAt35Pecent) {
+			if (taxableIncome >= taxAt35Percent) {
 				taxRate = 0.35;
-				incomeUntaxed = taxAt35Pecent - 1;
+				incomeUntaxed = taxAt35Percent - 1;
 			}
-			else if (taxableIncome >= taxAt33Pecent) {
+			else if (taxableIncome >= taxAt33Percent) {
 				taxRate = 0.33;
-				incomeUntaxed = taxAt33Pecent - 1;
+				incomeUntaxed = taxAt33Percent - 1;
 			}
-			else if (taxableIncome >= taxAt28Pecent) {
+			else if (taxableIncome >= taxAt28Percent) {
 				taxRate = 0.28;
-				incomeUntaxed = taxAt28Pecent - 1;
+				incomeUntaxed = taxAt28Percent - 1;
 			}
-			else if (taxableIncome >= taxAt25Pecent) {
+			else if (taxableIncome >= taxAt25Percent) {
 				taxRate = 0.25;
-				incomeUntaxed = taxAt25Pecent -1;
+				incomeUntaxed = taxAt25Percent -1;
 			}
-			else if (taxableIncome >= taxAt15Pecent) {
+			else if (taxableIncome >= taxAt15Percent) {
 				taxRate = 0.15;
-				incomeUntaxed = taxAt15Pecent - 1;
-			}
+				incomeUntaxed = taxAt15Percent - 1;
+			} 
 
 			// Accumulate tax for income above the 10% tax bracket 
 			tax += (incomeTaxed = taxableIncome - incomeUntaxed) * taxRate;
--- src/chapter_6/Exercise_6_16.java
+++ src/chapter_6/Exercise_6_16.java
@@ -12,7 +12,40 @@ package chapter_6;
  * @author lucasmaximo
  *
  */
+
+import java.util.Scanner;
+
 /** Create a exercise_6_16 class */
 public class Exercise_6_16 {
 
+	/** Create the main method */
+	public static void main (String[] args) {
+		
+		/** Created the objects */
+		Scanner input = new Scanner (System.in);
+		
+		/** Print the interaction with the user */
+		System.out.print("\n\n\tType the year to be calculate number of the days -> ");
+		/** Created the variable to absorb the user interaction */
+		int year = input.nextInt();
+		
+		/** print out the message calling the method to count days of the year */
+		System.out.print("\n\n\tThis year has " + numberOfDaysInAYear(year) + " days.");
+
+		input.close();
+	}//closing main method
+	
+	
+	/** Created a method following the instructions */
+	public static int numberOfDaysInAYear(int year) {
+		
+		//create an if statement in order to see if is or not a leap year
+		if ((year % 4) == 0 && (year % 100) == 0 && (year % 400) == 0 ) {
+			return 366;
+		}else {
+			return 365;
+		}//close if statement
+		
+	}//close a plublic method numberOfDaysInYear
+	
 }//close class_6_16
