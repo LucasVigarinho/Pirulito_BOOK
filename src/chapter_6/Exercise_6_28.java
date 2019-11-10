a50f8bf99cdbc1565a47e7225648ef6e43976cd2
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-11-10 13:13:02 +0000

    commit @LucasVigarinho exercise_6_28 solved

--- src/chapter_6/Exercise_6_28.java
+++ src/chapter_6/Exercise_6_28.java
@@ -15,6 +15,24 @@ package chapter_6;
  * @author lucasmaximo
  *
  */
+/** import the necessary libraries */
+
+/** Created the exercise_6_28 class*/ 
 public class Exercise_6_28 {
 
-}
+	/** Created the main method */
+	public static void main(String[] args) {
+		//created the necessary variables
+		int count = 0;
+		//print out the ennunciate
+		System.out.print("\n\n\titem\tp\t2ˆp -1");
+		for (int i = 0; count < 31; i++) {
+			if(Exercise_6_27.isPrime(i) && Exercise_6_27.isPrime((int) (Math.pow(2, i) -1))) {
+				count+=1;
+				System.out.print("\n\t" + count + "\t"+ i + "\t" + (int) (Math.pow(2, i) -1));
+				
+			}
+		}
+	}//closing the main method
+	
+}//closing the exercise_6_28 class
