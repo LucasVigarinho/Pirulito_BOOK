7fe4979fdcf922cb120d34c684f07c7e6b12398a
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-11-10 13:17:28 +0000

    commit @LucasVigarinho exercise 6 29 solved

--- src/chapter_6/Exercise_6_29.java
+++ src/chapter_6/Exercise_6_29.java
@@ -14,6 +14,22 @@ package chapter_6;
  * @author lucasmaximo
  *
  */
+/** Created the exercise_6_29 class*/ 
 public class Exercise_6_29 {
 
-}
+	/** Created the main method */
+	public static void main(String[] args) {
+		//created the necessary variables
+		int count = 0;
+		//print out the ennunciate
+		System.out.print("\n\n\titem\tp\t2ˆp -1");
+		for (int i = 0; count < 1000; i++) {
+			if(Exercise_6_27.isPrime(i) && Exercise_6_27.isPrime(i+2)) {
+				count+=1;
+				System.out.print("\n\t" + count + "\t"+ i + "\t" + (i+2));
+				
+			}
+		}
+	}//closing the main method
+	
+}//closing the exercise_6_29 class
