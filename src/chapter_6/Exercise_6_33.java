b7da58b028c5a582f6e39c67b1adcb12cbac48e0
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-11-10 18:38:53 +0000

    commit @LucasVigarinho exercise_6_33 solved

--- src/chapter_6/Exercise_6_33.java
+++ src/chapter_6/Exercise_6_33.java
@@ -1,5 +1,6 @@
 package chapter_6;
 
+import java.text.SimpleDateFormat;
 /**
  * 
  * 		(Current date and time) Invoking System.currentTimeMillis() returns the
@@ -7,10 +8,30 @@ package chapter_6;
  * 		that displays the date and time. Here is a sample run:
  * 
  * 		Current date and time is May 16, 2012 10:34:23
- * 
+ *
  * @author lucasmaximo
  *
  */
+import java.util.Date;
+
+
+/** Created the exercise_6_33 class */
 public class Exercise_6_33 {
 
-}
+	/** Created the main method */
+	public static void main(String [] args) {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+		System.out.println("\n\n\t Current date and time is " + formatter.format(time()));
+	}//closing the main method
+
+	//**created the method to set the time */
+	public static Date time() {
+
+		long millis = System.currentTimeMillis();  
+
+		Date date2 = new Date(millis);  
+
+		return (date2);
+	}//close Time method
+
+}//closed the exercise_6_33
