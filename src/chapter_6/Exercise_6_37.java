948f2afe9e4056e32be8e6deca27a0fe46f94a12
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-11-12 17:43:47 +0000

    commit @LucasVigarinho exercise_6_37 solved

--- src/chapter_6/Exercise_6_37.java
+++ src/chapter_6/Exercise_6_37.java
@@ -20,6 +20,54 @@ package chapter_6;
  * @author lucasmaximo
  *
  */
+/** Importing the necessary libraries */
+import java.util.Scanner;
+
+/** Created the exercise class */
 public class Exercise_6_37 {
 
-}
+	/** Created the main method */
+	public static void main(String [] args) {
+
+		/** Created the objects necessary */
+		Scanner input = new Scanner(System.in);
+
+		/** Print out the message to be the guide for the user interaction number */
+		System.out.print("\n\n\tType her the number -> ");
+		/** Created the variables necessary to absorb the user interaction number */
+		int number = input.nextInt();
+		/** Print out the message to be the guide for the user interaction width fo the number */
+		System.out.print("\n\tType her the size  -> ");
+		/** Created the variables necessary to absorb the user interaction width of the number */
+		int width = input.nextInt();
+
+		
+		/** Calling the method format */
+		System.out.print("\n\tThe number formated is -> " + format(number, width));
+		
+		input.close();
+	}//closing the main method
+
+	/** Created the method following the instructions */
+	public static String format(int number, int width) {
+
+		/** Created the necessary variables */
+		String form = "";
+		String test = String.valueOf(number);
+		
+		/** Created a if statement in order to not apply "0" when is the width lower than the length of the number*/
+		if(width < test.length()) {
+			
+		}else{
+			/** Created a for loop to apply the "0" with the limit of the length of the number */
+			for(int i = 0; i < width - test.length(); i++) {
+				form += "0";
+			}//closing for loop
+		}//closing if statement
+		
+		//apply the number after the string with the zeros applied
+		form += number;
+		return form;
+	}//close the method
+	
+}//closing class
