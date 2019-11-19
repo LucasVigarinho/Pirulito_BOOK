3d3d223d4f2a6617048aafe6c43627dadfd3bb61
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-11-19 21:54:45 +0000

    commit @LucasVigarinho exercise_7_08 solved

--- src/chapter_7/Exercise_7_08.java
+++ src/chapter_7/Exercise_7_08.java
@@ -1,7 +1,15 @@
 package chapter_7;
 /** 
  * 
+ * 		(Average an array) Write two overloaded methods that return the average of an
+ * 		array with the following headers:
  * 
+ * 		public static int average(int[] array)
+ * 
+ * 		public static double average(double[] array)
+ * 
+ * 		Write a test program that prompts the user to enter ten double values, invokes this
+ * 		method, and displays the average value.
  * 		
  * 		
  * 		
@@ -10,7 +18,7 @@ package chapter_7;
  */
 
 /** Necessary imports */
-
+import java.util.Scanner;
 
 /** Creating the class named Exercise_7_08 */
 public class Exercise_7_08{
@@ -19,15 +27,48 @@ public class Exercise_7_08{
 	public static void main(String[] arg) {
 
 		//create the necessary objects
+		Scanner input = new Scanner(System.in);
 
-
-		//create a for loop in order to test all the letters
-		//close for loop
-		//print out the messages 
+		//created the necessary variables
+		double [] numbers = new double[10];
 		
+		//printed out the message to guide the user interaction
+		System.out.print("\n\n\tInsert 10 double numbers -> ");
+		
+		//created a for loop in order to absorb all the user insertions 
+		for (int count = 0; count < 10; count++) {
+			numbers[count] = input.nextDouble();
+			
+		}//closing for loop
 	
+		//print out the average number using the second method
+		System.out.print("\n\n\tAverage number -> " + average(numbers));
+	
+
 	}//closing the main method
 
 
-
+	/** Created the first method following the instructions - method to be overload */
+	public static int average(int[] array) {
+		int sum = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		
+		return sum/array.length;
+		
+	}//closing the first method
+	
+	/** Created the second and the overloaded method following the instrutions */
+	public static double average(double[] array) {
+		
+		double sum = 0.00;
+		
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		
+		return sum/array.length;
+	}//closing the second and overloaded method
 }//closing the class_7_08
