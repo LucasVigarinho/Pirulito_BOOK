1151033eedf2997c7eaffa9b1e01125315428d35
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-12-15 15:36:02 +0000

    commit @LucasVigarinho exercise_8_010 solved

--- src/chapter_8/Exercise_8_10.java
+++ src/chapter_8/Exercise_8_10.java
@@ -19,7 +19,7 @@ package chapter_8;
  */
 
 /** Necessary imports */
-import java.util.Scanner;
+
 
 /** Creating the class named Exercise_8_01 */
 public class Exercise_8_10{
@@ -27,14 +27,78 @@ public class Exercise_8_10{
 	/** Creating the main method */
 	public static void main(String[] arg) {
 
-		//create the necessary objects
-		Scanner input = new Scanner (System.in);
+		//created the necessary variable array 4-by-4
+		int [][] matrix = new int [4][4];
+		int countRow = 0, countColumn = 0, lastCountRow = 0, lastCountColumn  = 0, row = 0, column = 0;
 
-		input.close();
+		
+
+		//created a for loop in order to fill the array created
+		for(int a = 0; a < matrix.length; a++) {
+			//created a nested for loop in order to fill all the positions of the matrix and count the numbers of 1's
+			for(int b = 0; b < matrix[a].length; b++) {
+				matrix[a][b] = (int) (Math.random()*2);
+			}//closing nested for loop
+		}//closing main for loop
+
+		printArray(matrix);
+
+		//created a for loop in order to fill the array created
+		for(int c = 0; c < matrix.length; c++) {
+			//created a nested for loop in order to fill all the positions of the matrix and count the numbers of 1's
+			for(int d = 0; d < matrix[c].length; d++) {
+
+				//created an if statement to count numbers of one in the row
+				if(matrix[c][d] == 1) {
+					countRow++;
+				}//closing if statement
+
+				//created an if statement to count numbers of one in the row
+				if(matrix[d][c] == 1) {
+					countColumn++;
+				}//closing if statement
+
+			}//closing nested loop
+
+			//created an if statement in order to check the sum of row
+			if(countRow > lastCountRow) {
+				lastCountRow = countRow;
+				countRow = 0;
+				row = c;
+			}else {
+				countRow = 0;
+			}//closing if for the row
+
+			//created an if statement in order to check the sum of columns
+			if(countColumn > lastCountColumn) {
+				lastCountColumn = countColumn;
+				countColumn = 0;
+				column = c;
+			}else {
+				countColumn = 0;
+			}//closing if for the columns
+
+		}//closing main for loop
+
+		System.out.print("\n\n\tThe largest row index: " + row);
+		System.out.print("\n\n\tThe largest column index: " + column);
 	}//closing the main method
 
-	
 
+	public static void printArray(int matrix[][]) {
+		System.out.print("\n\t");
+		//created a for loop in order to fill the array created
+		for(int a = 0; a < matrix.length; a++) {
+			//created a nested for loop in order to fill all the positions of the matrix and count the numbers of 1's
+			for(int b = 0; b < matrix[a].length; b++) {
+				System.out.print(matrix[a][b] + " ");
+			}//closing nested for loop
+			System.out.print("\n\t");
+
+		}//closing main for loop
+
+
+	}
 
 }//closing the class_8_01
 
