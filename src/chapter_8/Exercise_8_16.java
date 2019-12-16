c8a9fb4c3677ce118a7724ee77bdbb356e4a217f
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-12-16 23:45:33 +0000

    commit @LucasVigarinho Exercise_8_16 solved

--- src/chapter_8/Exercise_8_16.java
+++ src/chapter_8/Exercise_8_16.java
@@ -14,14 +14,19 @@ package chapter_8;
  * 
  * 		will be sorted to
  * 
- * 		{{1, 1},{1, 2},{1, 7},{4, 1},{4, 2},{4, 5}}.
+ * 		{{1, 1},
+ * 		 {1, 2},
+ * 		 {1, 7},
+ * 		 {4, 1},
+ * 		 {4, 2},
+ * 		 {4, 5}}.
  * 		
  * @author lucasmaximo
  *
  */
 
 /** Necessary imports */
-import java.util.Scanner;
+
 
 /** Creating the class named Exercise_8_01 */
 public class Exercise_8_16{
@@ -29,14 +34,64 @@ public class Exercise_8_16{
 	/** Creating the main method */
 	public static void main(String[] arg) {
 
-		//create the necessary objects
-		Scanner input = new Scanner (System.in);
+		// Created a multidimensional array using the values tester from the initial instructions
+		int[][] matrix = {{4, 2},{1, 7},{4, 5},{1, 2},{1, 1},{4, 1}};
 
-		input.close();
-	}//closing the main method
+		//printing out the matrix before sort it
+		print(matrix);
+		// call the method in order to sort the multidimensional array
+		sort(matrix);
+		//printing out the matrix after sort it
+		print(matrix);
 
-	
+	}
 
+	// Created a method following the initial introduction sorting the row and the columns  */
+	public static void sort(int [][] matrix) {
+
+		//created an array in order to absorb values and check in each loop
+		int[] min = new int[2];
+
+		//created a main for loop in order to run each row
+		for (int row = 0; row < matrix.length - 1; row++) {
+			//absorb the value of the position in the matrix put into the simple array to be tested
+			min[0] = matrix[row][0]; 
+			min[1] = matrix[row][1]; 
+			//created the index row
+			int index = row;
+
+			//created the nested for loop in order to check all the columns from each row and test the positions from the simple array created 
+			for (int i = row + 1; i < matrix.length; i++) {
+
+				//created an if statement to test the first and the second position in comparison to the columns 0  and the column 1 following the example in the instruction
+				if (matrix[i][0] <= min[0] && (matrix[i][0] < min[1] || matrix[i][1] < min[0] || matrix[i][1] < min[1])) { 
+					min[0] = matrix[i][0];
+					min[1] = matrix[i][1];
+					index = i;
+				}//closing if statement
+			}//closing nested for loop
+			
+			//if the index of the row is different them the row it self, change positions values
+			if (index != row){
+				matrix[index][0] = matrix[row][0];
+				matrix[index][1] = matrix[row][1];
+				matrix[row][0] = min[0];
+				matrix[row][1] = min[1]; 
+			}//closing if statement
+		}//closing main for loop
+	}//closing sort method
+
+
+	public static void print(int [][]matrix) {
+		System.out.print("\n\n\t");
+		// Display sorted array
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.print("\n\t");
+		}
+	}
 
 }//closing the class_8_01
 
