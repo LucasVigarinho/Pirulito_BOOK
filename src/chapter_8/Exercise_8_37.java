d5d900cfde6cf3c68a871b74f1520da55825ba51
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-12-21 18:07:01 +0000

    commit @LucasVigarinho Exercise_8_37 commented

--- src/chapter_8/Exercise_8_37.java
+++ src/chapter_8/Exercise_8_37.java
@@ -40,11 +40,102 @@ public class Exercise_8_37{
 	public static void main(String[] arg) {
 
 		//create the necessary objects
-		Scanner input = new Scanner (System.in);
+		Scanner input = new Scanner(System.in);
 
-		input.close();
-	}//closing the main method
+		// Created the multidimensional array calling the get states and cities method 
+		String[][] statesAndCapitals = getStatesAndCities();
 
+		int count = 0;			
+		
+		//created a for loop in order to questioning all the positions of the array created and count the result of correct answers
+		for (int i = 0; i < statesAndCapitals.length; i++) {
+			System.out.print("\n\tWhat is the capital of " + statesAndCapitals[i][0] + "? ");
+			String capital = input.nextLine();
+
+			//created an if statement in order to compare values of answer and array, using a boolean method
+			if (isEqual(statesAndCapitals[i][1], capital)) {
+				System.out.print("\n\tYour answer is correct");
+				count++;
+			}else {
+				System.out.print("\n\tThe correct answer should be " + 
+					statesAndCapitals[i][1]);
+			}//closing if statement
+		}//closing a for loop
+
+		// Display the total correct count
+		System.out.print("\n\tThe correct count is " + count);
+	}//closing a main method
+
+	/**  Created a method in order to verify if the answer of in order*/
+	public static boolean isEqual(String c, String a) {
+		//created an if statement in order to verify the length and if is not the same size, already return false
+		if (c.length() != a.length())
+			return false;
+
+		//created a for loop in order to verify if each variable is equal
+		for (int i = 0; i < c.length(); i++) {
+			if (c.charAt(i) != a.charAt(i))
+				return false;
+		}//closing for loop
+
+		return true;
+	}//closing method isEqual
+
+	/** Created a method get states and cities in order to initializes the array with the 50 states and their capitals */
+	public static String[][] getStatesAndCities() {
+		String[][] d = {
+			{	"Alabama"		,	"Montgomery"	}, 
+			{	"Alaska"		, 	"Juneau"		}, 
+			{	"Arizona"		, 	"Phoenix"		},
+			{	"Arkansas"		, 	"Little Rock"	}, 
+			{	"California"	, 	"Sacramento"	},
+			{	"Colorado"		, 	"Denver"		}, 
+			{	"Connecticut"	, 	"Hartford"		}, 
+			{	"Delaware"		, 	"Dover"			}, 
+			{	"Florida"		, 	"Tallahassee"	}, 
+			{	"Georgia"		,	"Atlanta"		},
+			{	"Hawaii"		, 	"Honolulu"		}, 
+			{	"Idaho"			, 	"Boise"			},
+			{	"Illinois"		, 	"Springfield"	}, 
+			{	"Indiana"		, 	"Indianapolis"	},
+			{	"Iowa Des"		, 	"Moines"		}, 
+			{	"Kansas"		, 	"Topeka"		}, 
+			{	"Kentucky"		,	"Frankfort"		}, 
+			{	"Louisiana"		, 	"Baton Rouge"	}, 
+			{	"Maine"			, 	"Augusta"		}, 
+			{	"Maryland"		, 	"Annapolis"		}, 
+			{	"Massachusetts"	, 	"Boston"		}, 
+			{	"Michigan"		, 	"Lansing"		}, 
+			{	"Minnesota"		, 	"Saint Paul"	}, 
+			{	"Mississippi"	, 	"Jackson"		}, 
+			{	"Missouri"		, 	"Jefferson City"}, 
+			{	"Montana"		, 	"Helena"		}, 
+			{	"Nebraska"		, 	"Lincoln"		}, 
+			{	"Nevada"		, 	"Carson City"	}, 
+			{	"New Hampshire"	, 	"Concord"		}, 
+			{	"New Jersey"	, 	"Trenton"		}, 
+			{	"New Mexico"	, 	"Santa Fe"		}, 
+			{	"New York"		, 	"Albany"		}, 
+			{	"North Carolina",	"Raleigh"		}, 
+			{	"North Dakota"	, 	"Bismarck"		},
+			{	"Ohio"			, 	"Columbus"		},
+			{	"Oklahoma"		, 	"Oklahoma City"	}, 
+			{	"Oregon"		, 	"Salem"			}, 
+			{	"Pennsylvania"	, 	"Harrisburg"	}, 
+			{	"Rhode Island"	, 	"Providence"	}, 
+			{	"South Carolina", 	"Columbia"		}, 
+			{	"South Dakota"	, 	"Pierre"		}, 
+			{	"Tennessee"		, 	"Nashville"		}, 
+			{	"Texas"			, 	"Austin"		}, 
+			{	"Utah"			, 	"Salt Lake City"}, 
+			{	"Vermont"		, 	"Montpelier"	}, 
+			{	"Virginia"		, 	"Richmond"		}, 
+			{	"Washington"	, 	"Olympia"		}, 
+			{	"West Virginia"	, 	"Charleston"	}, 
+			{	"Wisconsin"		, 	"Madison"		}, 
+			{	"Wyoming"		, 	"Cheyenne"		}};
+		return d;
+	}//closing the method get states and cities
 	
 
 
