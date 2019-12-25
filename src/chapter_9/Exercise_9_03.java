1a58471e2ac74b4aa540f8b32489c27784712448
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-12-25 19:25:31 +0000

    commit @LucasVigarinho Exercise 9_03 hald-solved

--- src/chapter_9/Exercise_9_03.java
+++ src/chapter_9/Exercise_9_03.java
@@ -12,7 +12,7 @@ package chapter_9;
  */
 
 /** Necessary imports */
-
+import java.util.Date;
 
 /** Creating the class named Exercise_9_03 */
 public class Exercise_9_03{
@@ -20,11 +20,52 @@ public class Exercise_9_03{
 	/** Creating the main method */
 	public static void main(String[] arg) {
 
-		//create the necessary variable
+		//create the first object using the library date, using 10000
+		Date date1 = new Date(10000);
+		//printing out the date from 1970 using toString() method
+		System.out.print("\n\n\t"+ date1.toString());
 
+		//create the second object using the library date, using 10000
+		Date date2 = new Date(100000);
+		//printing out the date from 1970 using toString() method
+		System.out.print("\n\n\t"+ date2.toString());
+
+		//create the third object using the library date, using 10000
+		Date date3 = new Date(1000000);
+		//printing out the date from 1970 using toString() method
+		System.out.print("\n\n\t"+ date3.toString());
+
+		//create the fourth object using the library date, using 10000
+		Date date4 = new Date(10000000);
+		//printing out the date from 1970 using toString() method
+		System.out.print("\n\n\t"+ date4.toString());
+
+		//create the fifth object using the library date, using 10000
+		Date date5 = new Date(100000000);
+		//printing out the date from 1970 using toString() method
+		System.out.print("\n\n\t"+ date5.toString());
+
+		//create the sixth object using the library date, using 10000
+		Date date6 = new Date(1000000000);
+		//printing out the date from 1970 using toString() method
+		System.out.print("\n\n\t"+ date6.toString());
+		
+		//out of range - check on Internet possible solutions
+
+//		//create the seventh object using the library date, using 10000
+//		Date date7 = new Date(10000000000);
+//		//printing out the date from 1970 using toString() method
+//		System.out.print("\n\n\t"+ date7.toString());
+//
+//		//create the eight object using the library date, using 10000
+//		Date date8 = new Date(100000000000);
+//		//printing out the date from 1970 using toString() method
+//		System.out.print("\n\n\t"+ date8.toString());
 
 	}//closing the main method
 
-	
+
 }//closing the class_9_03
+
+
 
