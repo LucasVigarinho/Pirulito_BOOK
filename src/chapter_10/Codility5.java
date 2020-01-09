aaf45f1c65d66ec7af9806d3f5a94f301082b273
LucasVigarinho <lucas.maximo.mv@gmail.com>
2020-01-09 23:08:57 +0000

    commit @LucasVigarinho codiliity exercises

---
+++ src/chapter_10/Codility5.java
@@ -0,0 +1,64 @@
+package chapter_10;
+
+public class Codility5 {
+	public static void main(String[] args){
+		System.out.println(solution(268));
+		System.out.println(solution(670));
+		System.out.println(solution(0));
+		System.out.println(solution(-999));
+
+	}
+
+	public static int solution(int N){
+		
+		boolean isNegative = false;
+
+		if (N < 0){
+			
+			isNegative = true;
+			
+			N = N * (-1);
+		}
+
+		String transform = String.valueOf(N);
+	
+		int[]intTab = new int[transform.length()];
+		
+		for (int i = 0; i < intTab.length; i++){
+			String temp1 = Character.toString(transform.charAt(i));
+			int temp2 = Integer.parseInt(temp1);
+			intTab[i] = temp2;
+		}
+		
+		int[] newNumber = new int[intTab.length + 1];
+
+		if (isNegative){
+			newNumber[0] = 5;
+			for (int i = 0; i < intTab.length; i++){
+				newNumber[i + 1] = intTab[i];
+			}
+		}else{
+			for (int i = 0; i < intTab.length; i++){
+				if (5 > intTab[i]){
+					newNumber[i] = 5;
+					for (int j = i; j < intTab.length; j++){
+						newNumber[j + 1] = intTab[j];
+					}
+					break;
+				}
+				else{
+					newNumber[i] = intTab[i];
+				}
+			}
+		}
+
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < newNumber.length; i++){
+			sb.append(newNumber[i]);
+		}
+
+		int retorno = Integer.parseInt(sb.toString());
+		return isNegative ? (retorno * (-1)) : retorno;
+	}
+
+}
---
+++ src/chapter_10/CodilityString.java
@@ -0,0 +1,58 @@
+package chapter_10;
+
+
+
+public class CodilityString {
+	public static void main(String[] args){
+		StringBuffer sb = new StringBuffer();
+
+		sb.append("my.song.mp3 11b" + "\n");
+		sb.append("greatSong.flac 1000b" + "\n");
+		sb.append("not3.txt 5b" + "\n");
+		sb.append("video.mp4 200b" + "\n");
+		sb.append("game.exe 100b" + "\n");
+		sb.append("mov!e.mkv 10000b");
+
+		System.out.print(solution(sb.toString()));
+	}
+
+	public static String solution(String S){
+
+		String[] newone = S.split("\n");
+		int music = 0;
+		int images = 0;
+		int movies = 0;
+		int other = 0;
+		int lenght = newone.length;
+		for(int x = 0; x < lenght; x++) {
+			String[] item = newone[x].split(" ");
+			String[] extension = item[0].split("\\.");
+			String finalExtension = extension[extension.length - 1];
+			String[] size = item[1].split("b");
+			String finalSize = size[0];
+			
+			
+			if(finalExtension.equalsIgnoreCase("mp3") || finalExtension.equalsIgnoreCase("aac")|| finalExtension.equalsIgnoreCase("flac")) {
+				music += Integer.parseInt(finalSize);
+			}
+			else if(finalExtension.equalsIgnoreCase("jpg") || finalExtension.equalsIgnoreCase("bmp")|| finalExtension.equalsIgnoreCase("gif")) {
+				images += Integer.parseInt(finalSize);
+			}
+			else if(finalExtension.equalsIgnoreCase("mp4") || finalExtension.equalsIgnoreCase("avi")|| finalExtension.equalsIgnoreCase("mkv")) {
+				movies += Integer.parseInt(finalSize);
+			} else {
+				other += Integer.parseInt(finalSize);
+			}
+			
+		}
+		
+		
+		S = ("music " + music + "b\n" + "images " + images + "b\n" + "movies " + movies + "b\n" + "other " + other +"b");
+		
+		
+		
+		
+		return S;
+	}
+}
+
