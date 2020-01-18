4618a80d4597ff59439d8c0b23f6a5a7e4c2ee27
LucasVigarinho <lucas.maximo.mv@gmail.com>
2020-01-18 20:07:35 +0000

    commit @LucasVigarinho Exercise_10_16 solved)

--- src/chapter_10/Exercise_10_15.java
+++ src/chapter_10/Exercise_10_15.java
@@ -4,8 +4,11 @@ package chapter_10;
  * 			
  * 	(Geometry: the bounding rectangle) A bounding rectangle is the minimum rectangle
  * 	that encloses a set of points in a two-dimensional plane, as shown in
- * 	Figure 10.24d. Write a method that returns a bounding rectangle for a set of
+ * 	Figure 10.24d. 
+ * 		
+ * 	Write a method that returns a bounding rectangle for a set of
  * 	points in a two-dimensional plane, as follows:
+ * 	
  * 	public static MyRectangle2D getRectangle(double[][] points)
  * 		
  * 	The Rectangle2D class is defined in Programming Exercise 10.13. Write a
@@ -21,7 +24,7 @@ package chapter_10;
  */
 
 /** Necessary imports */
-
+import chapter_10.Exercise_10_13;
 
 /** Creating the class named Exercise_10_01 */
 public class Exercise_10_15{
@@ -30,11 +33,30 @@ public class Exercise_10_15{
 	public static void main(String[] arg) {
 
 		
+		/** Write a method that returns a bounding rectangle for a set of
+		 * 	points in a two-dimensional plane, as follows:
+		 * 	
+		 * 	public static MyRectangle2D getRectangle(double[][] points)
+		 * 		
+		 * 	The Rectangle2D class is defined in Programming Exercise 10.13. 
+		 * 
+		 * 	Write a test program that prompts the user to enter five points and displays the bounding
+		 * 	rectangle’s center, width, and height. Here is a sample run:*/	
 		
+		double [][] rectangle = new double [][] {	{1.0, 2.5	},
+													{3	, 4		},
+													{5	, 6		},
+													{7	, 8		},
+													{9	, 10	}};
+		getRectangle(rectangle);
 
 	}//closing the main method
 
-	
+	public static MyRectangle2D getRectangle(double[][] points) {
+		double width;
+		double height;
+		
+	}
 
 }//closing the Exercise_10_01 method (used as driver method)
 
--- src/chapter_10/Exercise_10_16.java
+++ src/chapter_10/Exercise_10_16.java
@@ -1,5 +1,8 @@
 package chapter_10;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /** 
  * 			
  * 	(Divisible by 2 or 3) Find the first ten numbers with 50 decimal digits that are
@@ -19,12 +22,29 @@ public class Exercise_10_16{
 	/** Creating the main method */
 	public static void main(String[] arg) {
 
-		
-		
+		BigInteger bigNumber = new BigInteger("100000000000000000000000000000000000000000000000000");
 
+		int count = 0;
+
+		do {
+			boolean temp2 = bigNumber.remainder(new BigInteger("2")).equals(BigInteger.ZERO);
+			boolean temp3 = bigNumber.remainder(new BigInteger("3")).equals(BigInteger.ZERO);
+			if(temp2 || temp3) {
+
+				count++;
+				System.out.print("\nNumber\t" + (count) + "\t->" + bigNumber);
+			}
+			bigNumber = bigNumber.add(BigInteger.ONE);
+
+		}while(count < 10);
+
+		
+		Double x = 3.5;
+		System.out.println(x.intValue());
+		System.out.println(x.compareTo(4.5));
 	}//closing the main method
 
-	
+
 
 }//closing the Exercise_10_01 method (used as driver method)
 
