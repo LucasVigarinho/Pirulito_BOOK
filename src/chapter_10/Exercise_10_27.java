006ac7b1d9b3470f6c6b488a7143ec16c5dd59c8
LucasVigarinho <lucas.maximo.mv@gmail.com>
2020-02-08 14:30:10 +0000

    commit @LucasVigarinho Exercise_10_27 solved

--- src/chapter_10/Exercise_10_27.java
+++ src/chapter_10/Exercise_10_27.java
@@ -21,15 +21,38 @@ package chapter_10;
 
 /** Necessary imports */
 
-
 /** Creating the class named Exercise_10_01 */
 public class Exercise_10_27{
 
 	/** Creating the main method */
 	public static void main(String[] arg) {
 
+		/** Testing all the methods */
+		/** public MyStringBuilder1(String s); */
+		MyStringBuilder1 test = new MyStringBuilder1("Lucas");
+		System.out.println(test.toString());
+//		/** public MyStringBuilder1 append(MyStringBuilder1 s);*/
+		test.append(new MyStringBuilder1 (" Maximo"));
+		System.out.println(test.toString());
+
+//		/**	public MyStringBuilder1 append(int i);*/
+		test.append(16);
+		System.out.println(test.toString());
+
+//		/**	public int length();*/
+		System.out.println(test.length());
 		
-		
+//		/**	public char charAt(int index);*/
+		System.out.println(test.charAt(01));
+
+//		/**	public MyStringBuilder1 toLowerCase();*/
+		System.out.println(test.toLowerCase().toString());
+	
+//		/**	public MyStringBuilder1 substring(int begin, int end);*/
+		System.out.println((test.substring(1, 5).toString()));
+//		
+//		/**	public String toString();*/
+//		System.out.print(test);
 
 	}//closing the main method
 
@@ -38,4 +61,66 @@ public class Exercise_10_27{
 }//closing the Exercise_10_01 method (used as driver method)
 
 
+/** (Implement the StringBuilder class) ...	(name the new class MyStringBuilder1): */
+class MyStringBuilder1{
 
+	//created the initial necessary variable
+	private String  chars;
+	
+/**	public MyStringBuilder1(String s); */
+	public MyStringBuilder1(String s) {
+		chars = s;
+	}
+	
+/** public MyStringBuilder1 append(MyStringBuilder1 s); */
+	public MyStringBuilder1 append(MyStringBuilder1 s) {
+		return new MyStringBuilder1(chars += s);
+	}
+	
+/** public MyStringBuilder1 append(int i); */
+	public MyStringBuilder1 append(int i) {
+		return append(new MyStringBuilder1(Character.toString( Character.valueOf((char)i))));
+	}
+/** public int length(); */
+	public int length() {
+		return chars.length();
+	}
+	
+/** public char charAt(int index); */
+	public char charAt(int index) {
+		
+		return chars.toCharArray()[index];
+	}
+
+/** public MyStringBuilder1 toLowerCase(); */
+	public MyStringBuilder1 toLowerCase() {
+		char [] lower = chars.toCharArray();
+		
+		for(int i = 0; i < lower.length; i++) {
+			if(lower[i] >= 'A' && lower[i] <='Z'){
+				lower[i] = (char) ((char) lower[i] + 32);
+			}
+		}
+		String ans = new String(lower);
+		return new MyStringBuilder1(ans);
+	}
+
+/** public MyStringBuilder1 substring(int begin, int end); */
+	public MyStringBuilder1 substring(int begin, int end) {
+		char[] subChar = new char [(end - begin)+1];
+//		char[] temp = chars.toCharArray();
+		for(int i = begin; i < end; i++ ) {
+			subChar[i] = chars.charAt(i);
+		}
+		String ans = new String(subChar);
+
+
+		return new MyStringBuilder1(ans);
+	}
+	@Override
+/** public String toString(); */
+	 public String toString() {
+		 return this.chars;
+	 }
+
+}//closed MyStringBuilder1 class
