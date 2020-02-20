8ae2f366258ffecf4e31a94dd07c73bbd2618eab
LucasVigarinho <lucas.maximo.mv@gmail.com>
2020-02-20 20:53:47 +0000

    Commit @LucasVigarinho Exercise_11_15 solved

--- src/chapter_11/Exercise_11_15.java
+++ src/chapter_11/Exercise_11_15.java
@@ -17,16 +17,61 @@ package chapter_11;
 /**			IMPORTED NECESSARY LIBRARIES 
  * */
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 
 /** Created the main class, using the name of the package (11) and the exercise (01) in a compose name */
 public class Exercise_11_15 {
-	
-	
+
+
 	/** Created the main method, in order to test all the methods that eventually has to be created and 
 	 * make possible the interaction with the user */
 	public static void main(String[] args) {
-		
-	}//closing the mains method
 
+		/** Created the Scanner object */
+		Scanner input = new Scanner(System.in);
+		/** Created the variable, to absorb the are of Polygon */
+		double area = getAreaOfConvexPolygon(input);
+
+		/** Print out the final answer */
+		System.out.println("The total area is " + area);
+
+	}//closing the mains method
 	
+	/** Created the getAreaOfConvexPolygon method, receiving the user input */
+	public static double getAreaOfConvexPolygon(Scanner input) {
+		/** Created the ArrayList of ArrayList double */
+		ArrayList<ArrayList<Double>> points = getPoints(input);
+
+		double sumOfProducts = 0.0;
+		
+		for (int i = 0; i < points.size() - 1; i++) {
+			double x1 = points.get(i).get(0);
+			double y1 = points.get(i).get(1);
+			double x2 = points.get(i + 1).get(0);
+			double y2 = points.get(i + 1).get(1);
+			sumOfProducts += ((x1 * y2) - (y1 * x2));
+		}
+
+		return Math.abs(sumOfProducts / 2);
+	}
+
+	/** Created the getPoints method, return an ArrayList of ArrayList of Double */
+	public static ArrayList<ArrayList<Double>> getPoints(Scanner input) {
+		System.out.print("Enter the number of the points: ");
+		int size = input.nextInt();
+		System.out.print("Enter the coordinates of the points: ");
+		ArrayList<ArrayList<Double>> points = new ArrayList<>();
+		for (int i = 0; i < size; i++) {
+			ArrayList<Double> point = new ArrayList<>();
+			point.add(input.nextDouble());
+			point.add(input.nextDouble());
+			points.add(point);
+		}
+		return points;
+	}
+
+
+
 }//closing class exercise_11_01
