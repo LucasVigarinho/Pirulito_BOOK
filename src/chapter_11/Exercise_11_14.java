93a616a81cc5dd37ac78a835da73d8e15a95cdaf
LucasVigarinho <lucas.maximo.mv@gmail.com>
2020-02-20 20:39:49 +0000

    Commit @LucasVigarinho Exercise_11_14 solved

--- src/chapter_11/Exercise_11_14.java
+++ src/chapter_11/Exercise_11_14.java
@@ -20,7 +20,11 @@ package chapter_11;
 
 /**			IMPORTED NECESSARY LIBRARIES 
  * */
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /** Created the main class, using the name of the package (11) and the exercise (01) in a compose name */
 public class Exercise_11_14 {
@@ -29,8 +33,48 @@ public class Exercise_11_14 {
 	/** Created the main method, in order to test all the methods that eventually has to be created and 
 	 * make possible the interaction with the user */
 	public static void main(String[] args) {
+		/** Created the object scanner to make possible the user interaction */
+		Scanner input = new Scanner (System.in);
 		
+		/** Created the arrayList in order to absorb the user interaction */
+		ArrayList <Integer> list1 = new ArrayList <>();
+		ArrayList <Integer> list2 = new ArrayList <>();
+
+
+		/** Printing out the message to be the user guide */
+		System.out.println("\n\t\t********** Are you going to type 5 numbers for the first list, in order to be union betweewn two ArrayList **********");
+		
+		insert(input, list1);
+		
+		System.out.println("\n\t\t********** Are you going to type 5 numbers for the second list, in order to be union betweewn two ArrayList **********");
+
+		insert(input, list2);
+
+		
+		ArrayList <Integer> unionOfLists = union(list1, list2);
+		
+		
+		System.out.print("\n\n\t\t" + Arrays.deepToString(unionOfLists.toArray()));
+		
+		input.close();
 	}//closing the mains method
 
+	/** created a method in order to insert the numbers inserted by the user into the list */
+	public static ArrayList<Integer> insert(Scanner input,  ArrayList <Integer> x ){
+		/** Created the for loop in order to receive the user interaction */
+		for(int i = 0; i < 5; i ++ ) {
+			System.out.print("\n\t\tPlease, insert the number -> ");
+			x.add(input.nextInt());
+		}//closing for loop 
+		
+		return x;
+	}
+	
+	/** Created a method union in order to unites the two lists */
+	public static ArrayList<Integer> union(ArrayList<Integer> list1, ArrayList<Integer> list2){
+		
+		return (ArrayList<Integer>) Stream.concat(list1.stream(), list2.stream()).collect(Collectors.toList());
+	}
 	
 }//closing class exercise_11_01
+
