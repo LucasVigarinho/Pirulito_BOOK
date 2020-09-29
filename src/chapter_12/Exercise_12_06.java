03fd7071fdcb2f5a887e2bcdf0d694c81cd200b6
LucasVigarinho <lucas.maximo.mv@gmail.com>
2020-09-29 15:29:48 +0100

    @LucasVigarinho

--- src/chapter_12/Exercise_12_06.java
+++ src/chapter_12/Exercise_12_06.java
@@ -1,5 +1,5 @@
 package chapter_12;
-
+import java.util.Scanner;
 
 /**			EXERCISE DESCRIPTION 
  * 
@@ -22,12 +22,61 @@ public class Exercise_12_06 {
 	public static void main(String[] args) {
 		
 		/** testing all the classes and methods */
+		//Creating a Scanner
+		Scanner input = new Scanner (System.in);
 		
-
-
-
+		//Giving to the user the opportunity to interact
+		System.out.println("\n\t" + "Enter a hex number: ");
+		String hex = input.nextLine();
 		
+		//creating a catch in order to handle the NumberFormatException
+		try {
+			System.out.println("The decimal value for hex number " +
+						hex + " is " + hexToDecimal(hex.toUpperCase());
+			
+		}catch (NumberFormatException ex){
+			System.out.println(ex.getMessage());
+		}//closing try catch
+		//Closing the input Scanner variable
+		input.close();
 	}//closing the mains method
 
+	/** Creating the class to be used as a method hexToDecimal */
+	public static int hexToDecimal (String hex) throws NumberFormatException{
+		
+		//create a variable in order to start the initial Decimal Value
+		int decimalValue = 0;
+		
+		//create a for loop in order to verify character by character of the user input
+		for (int i =0; i < hex.length(); i++) {
+			if ( !(hex.charAt(i) >= '0' && hex.charAt(i) <= '9') ||
+					!(hex.charAt(i) >= 'A' && hex.charAt(i) <= 'F')) {
+				
+						throw new  NumberFormatException ("String is not a hex String");
+			}
+			
+			//create a char variable to absorb the hex decimal number that passed through the if test above
+			char hexChar = hex.charAt(i);
+			
+			//using the int value created in order to compose the final hexdecimalvalue
+			decimalValue = decimalValue * 16 + hexCharToDecimal(hexChar);
+			
+		}//closing main loop
+		
+		return decimalValue;
+		
+	}//closing hexToDecimal class
+	
+	
+	//create a public class in order to transform the hex value to an int
+	public static int hexCharToDecimal(char ch) {
+		
+		if (ch >= 'A' && ch <= 'F') {
+			return 10 + ch - 'A';
+		}else {
+			return ch - '0';
+		}
+	}
+	
 	
 }//closing class exercise_12_06
